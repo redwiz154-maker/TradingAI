46cb90969d707c89a3acb45717fbff951d74f24b
Red Wiz <redwiz154@gmail.com>
2026-08-31 19:44:41 +0500

    Fix Capacitor web directory

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.redwiz.tradingai',
+  appName: 'TradingAI',
+  webDir: 'dist'
+};
+
+export default config;
